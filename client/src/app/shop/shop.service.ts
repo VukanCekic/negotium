5f908d43105a1f669d7d98919d18a108ed23ddca
Vukan <vukanwork@gmail.com>
2020-07-27 16:41:03 +0200

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IBrand } from '../shared/models/brands';
 import { IType } from '../shared/models/productType';
 import {map} from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -42,9 +43,14 @@ export class ShopService {
      );
   }
 
+  getProduct(id: number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands(){
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands')
   }
+
   
   getTypes(){
     return this.http.get<IType[]>(this.baseUrl + 'products/types')
